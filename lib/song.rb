26afc9add15ec34d3de249d4caba28ae146d7249
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-16 21:14:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,9 +23,8 @@ class Song
   end
   
   def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    @@all << song
+    song = self.new_by_name(name)
+    song.save
   end
   
   
